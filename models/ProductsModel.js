9095d9ee6f96f9a4c7712adf0b6240666d63258f
huuhao555 <huuhao555@gmail.com>
2024-10-12 14:49:05 +0700

    cap nhat model

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    ID_Product: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    ID_Product: { type: Number, required: true, unique: true },
     name: { type: String, required: true },
     description: { type: String },
     price: { type: Number, required: true },
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    ID_User: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    ID_User: { type: Number, required: true, unique: true },
     username: { type: String, required: true  },
     email: { type: String, required: true },
     phoneNumber: { type: String },
